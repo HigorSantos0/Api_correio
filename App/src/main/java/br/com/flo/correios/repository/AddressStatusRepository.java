e314f593a037294116523978be655b1d71fe3634
Higor do Nascimento Santos <higor.2120478300029@faeterj-rio.edu.br>
2023-12-18 17:06:19 -0300

    Add files via upload

---
+++ App/src/main/java/br/com/flo/correios/repository/AddressStatusRepository.java
@@ -0,0 +1,9 @@
+package br.com.flo.correios.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.flo.correios.model.AddressStatus;
+
+public interface AddressStatusRepository extends CrudRepository<AddressStatus, Integer>{
+
+}
