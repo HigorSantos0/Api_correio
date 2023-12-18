cb11bf1680866251f3c6dfc3bf7f6072d487730d
Higor do Nascimento Santos <higor.2120478300029@faeterj-rio.edu.br>
2023-12-18 17:02:52 -0300

    Update NoContentException.java

--- App/src/main/java/br/com/flo/correios/exception/NoContentException.java
+++ App/src/main/java/br/com/flo/correios/exception/NoContentException.java
@@ -1,1 +1,11 @@
+package br.com.flo.correios.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NO_CONTENT) //204
+public class NoContentException extends Exception{
+	
+
+}
 
